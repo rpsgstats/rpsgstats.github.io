27e517fc70f0ed8b31a727fd0876b187aafae088
rpsgstats <creepz1353@gmail.com>
2026-05-26 20:09:26 -0400

    Update player.js

--- player.js
+++ player.js
@@ -1,1 +1,322 @@
+const players = {
 
+"l1ght": {
+rank:"Bronze I",
+points:0,
+goals:0,
+assists:0,
+rating:0,
+value:"£0",
+bio:"No bio yet.",
+link:"#"
+},
+
+"ado": {
+rank:"Bronze I",
+points:0,
+goals:0,
+assists:0,
+rating:0,
+value:"£0",
+bio:"No bio yet.",
+link:"#"
+},
+
+"adrian": {
+rank:"Bronze I",
+points:0,
+goals:0,
+assists:0,
+rating:0,
+value:"£0",
+bio:"No bio yet.",
+link:"#"
+},
+
+"ammar": {
+rank:"Bronze I",
+points:0,
+goals:0,
+assists:0,
+rating:0,
+value:"£0",
+bio:"No bio yet.",
+link:"#"
+},
+
+".astroflow!": {
+rank:"Bronze I",
+points:0,
+goals:0,
+assists:0,
+rating:0,
+value:"£0",
+bio:"No bio yet.",
+link:"#"
+},
+
+"Fart poo 42": {
+rank:"Bronze I",
+points:0,
+goals:0,
+assists:0,
+rating:0,
+value:"£0",
+bio:"No bio yet.",
+link:"#"
+},
+
+"sweatyballs.VRFS": {
+rank:"Bronze I",
+points:0,
+goals:0,
+assists:0,
+rating:0,
+value:"£0",
+bio:"No bio yet.",
+link:"#"
+},
+
+"Luki": {
+rank:"Bronze I",
+points:0,
+goals:0,
+assists:0,
+rating:0,
+value:"£0",
+bio:"No bio yet.",
+link:"#"
+},
+
+"ccxze": {
+rank:"Bronze I",
+points:0,
+goals:0,
+assists:0,
+rating:0,
+value:"£0",
+bio:"No bio yet.",
+link:"#"
+},
+
+"bebeh=sumo": {
+rank:"Bronze I",
+points:0,
+goals:0,
+assists:0,
+rating:0,
+value:"£0",
+bio:"No bio yet.",
+link:"#"
+},
+
+"batata jr": {
+rank:"Bronze I",
+points:0,
+goals:0,
+assists:0,
+rating:0,
+value:"£0",
+bio:"No bio yet.",
+link:"#"
+},
+
+"diablo.tg": {
+rank:"Bronze I",
+points:0,
+goals:0,
+assists:0,
+rating:0,
+value:"£0",
+bio:"No bio yet.",
+link:"#"
+},
+
+"DM.Vrfs": {
+rank:"Bronze I",
+points:0,
+goals:0,
+assists:0,
+rating:0,
+value:"£0",
+bio:"No bio yet.",
+link:"#"
+},
+
+"Dutch.": {
+rank:"Bronze I",
+points:0,
+goals:0,
+assists:0,
+rating:0,
+value:"£0",
+bio:"No bio yet.",
+link:"#"
+},
+
+"chico": {
+rank:"Bronze I",
+points:0,
+goals:0,
+assists:0,
+rating:0,
+value:"£0",
+bio:"No bio yet.",
+link:"#"
+},
+
+"vinassotuff": {
+rank:"Bronze I",
+points:0,
+goals:0,
+assists:0,
+rating:0,
+value:"£0",
+bio:"No bio yet.",
+link:"#"
+},
+
+"Arditaaaa": {
+rank:"Bronze I",
+points:0,
+goals:0,
+assists:0,
+rating:0,
+value:"£0",
+bio:"No bio yet.",
+link:"#"
+},
+
+"Zta.": {
+rank:"Bronze I",
+points:0,
+goals:0,
+assists:0,
+rating:0,
+value:"£0",
+bio:"No bio yet.",
+link:"#"
+},
+
+"Goob": {
+rank:"Bronze I",
+points:0,
+goals:0,
+assists:0,
+rating:0,
+value:"£0",
+bio:"No bio yet.",
+link:"#"
+},
+
+"el bicho": {
+rank:"Bronze I",
+points:0,
+goals:0,
+assists:0,
+rating:0,
+value:"£0",
+bio:"No bio yet.",
+link:"#"
+},
+
+"Hybrid9111": {
+rank:"Bronze I",
+points:0,
+goals:0,
+assists:0,
+rating:0,
+value:"£0",
+bio:"No bio yet.",
+link:"#"
+},
+
+"Hyper. Vrfs pro": {
+rank:"Bronze I",
+points:0,
+goals:0,
+assists:0,
+rating:0,
+value:"£0",
+bio:"No bio yet.",
+link:"#"
+},
+
+"Brody": {
+rank:"Bronze I",
+points:0,
+goals:0,
+assists:0,
+rating:0,
+value:"£0",
+bio:"No bio yet.",
+link:"#"
+},
+
+"JB-KAFZ": {
+rank:"Bronze I",
+points:0,
+goals:0,
+assists:0,
+rating:0,
+value:"£0",
+bio:"No bio yet.",
+link:"#"
+},
+
+"JT.vrfs": {
+rank:"Bronze I",
+points:0,
+goals:0,
+assists:0,
+rating:0,
+value:"£0",
+bio:"RPSG CB",
+link:"#"
+},
+
+"ZZZ": {
+rank:"Bronze I",
+points:0,
+goals:0,
+assists:0,
+rating:0,
+value:"£0",
+bio:"No bio yet.",
+link:"#"
+},
+
+"Kylian mbappe": {
+rank:"Bronze I",
+points:0,
+goals:0,
+assists:0,
+rating:0,
+value:"£0",
+bio:"No bio yet.",
+link:"#"
+},
+
+"Lucas": {
+rank:"Bronze I",
+points:0,
+goals:0,
+assists:0,
+rating:0,
+value:"£0",
+bio:"No bio yet.",
+link:"#"
+},
+
+"ET9": {
+rank:"Bronze I",
+points:0,
+goals:0,
+assists:0,
+rating:0,
+value:"£0",
+bio:"No bio yet.",
+link:"#"
+}
+
+};
