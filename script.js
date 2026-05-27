fb9fec8fbb5b6f39c8c861e2a84ae867ffab4d13
rpsgvrfsstats <creepz1353@gmail.com>
2026-05-26 21:29:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,113 @@
 
-document.getElementById("playerBio")
-.innerText = players[playerName].bio;
+const playerList =
+document.getElementById("playerList");
 
-document.getElementById("playerLink")
-.href = players[playerName].link;
+const colors = [
+"red",
+"blue",
+"green",
+"orange",
+"purple"
+];
+
+const positions = [
+"Goalkeeper",
+"Defender",
+"Midfielder",
+"Attacker",
+"Striker"
+];
+
+players.forEach((player,index)=>{
+
+const color =
+colors[index % colors.length];
+
+const position =
+positions[index % positions.length];
+
+const initials =
+player.substring(0,2).toUpperCase();
+
+playerList.innerHTML += `
+
+<div class="player-card"
+onclick="window.location='player.html?name=${encodeURIComponent(player)}'">
+
+<div class="player-left">
+
+<div class="player-pfp ${color}">
+${initials}
+</div>
+
+<div>
+
+<div class="player-name">
+${player}
+</div>
+
+<div class="player-row">
+
+<div class="rank">
+Bronze I
+</div>
+
+<div class="position">
+${position}
+</div>
+
+</div>
+
+<div class="player-stats">
+0G • 0A • 0CS • 0B
+</div>
+
+</div>
+
+</div>
+
+<div class="player-right">
+
+<div class="points">
+0
+</div>
+
+<div class="pts">
+PTS
+</div>
+
+</div>
+
+</div>
+
+`;
+
+});
+
+/* SEARCH */
+
+document.getElementById("search")
+.addEventListener("input",e=>{
+
+let value =
+e.target.value.toLowerCase();
+
+let cards =
+document.querySelectorAll(".player-card");
+
+cards.forEach(card=>{
+
+let name =
+card.querySelector(".player-name")
+.innerText.toLowerCase();
+
+if(name.includes(value)){
+card.style.display="flex";
+}
+else{
+card.style.display="none";
+}
+
+});
+
+});
