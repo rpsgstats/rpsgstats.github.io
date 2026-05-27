0f7dc67e437e74d9227ba5e487f657bbc78aaed9
rpsgvrfsstats <creepz1353@gmail.com>
2026-05-26 23:47:46 -0400

    Create players.js

---
+++ players.js
@@ -0,0 +1,553 @@
+const players = {
+
+"l1ght":{
+rank:"Bronze I",
+position:"Attacker",
+points:0,
+goals:0,
+assists:0,
+blocks:0,
+cleansheets:0,
+value:"£0"
+},
+
+"ado":{
+rank:"Bronze I",
+position:"Defender",
+points:0,
+goals:0,
+assists:0,
+blocks:0,
+cleansheets:0,
+value:"£0"
+},
+
+"adrian":{
+rank:"Bronze I",
+position:"Midfielder",
+points:0,
+goals:0,
+assists:0,
+blocks:0,
+cleansheets:0,
+value:"£0"
+},
+
+"ammar":{
+rank:"Bronze I",
+position:"Goalkeeper",
+points:0,
+goals:0,
+assists:0,
+blocks:0,
+cleansheets:0,
+value:"£0"
+},
+
+".astroflow!":{
+rank:"Bronze I",
+position:"Attacker",
+points:0,
+goals:0,
+assists:0,
+blocks:0,
+cleansheets:0,
+value:"£0"
+},
+
+"Fart poo 42":{
+rank:"Bronze I",
+position:"Defender",
+points:0,
+goals:0,
+assists:0,
+blocks:0,
+cleansheets:0,
+value:"£0"
+},
+
+"sweatyballs.VRFS":{
+rank:"Bronze I",
+position:"Midfielder",
+points:0,
+goals:0,
+assists:0,
+blocks:0,
+cleansheets:0,
+value:"£0"
+},
+
+"Luki":{
+rank:"Bronze I",
+position:"Attacker",
+points:0,
+goals:0,
+assists:0,
+blocks:0,
+cleansheets:0,
+value:"£0"
+},
+
+"ccxze":{
+rank:"Bronze I",
+position:"Defender",
+points:0,
+goals:0,
+assists:0,
+blocks:0,
+cleansheets:0,
+value:"£0"
+},
+
+"bebeh=sumo":{
+rank:"Bronze I",
+position:"Goalkeeper",
+points:0,
+goals:0,
+assists:0,
+blocks:0,
+cleansheets:0,
+value:"£0"
+},
+
+"batata jr":{
+rank:"Bronze I",
+position:"Midfielder",
+points:0,
+goals:0,
+assists:0,
+blocks:0,
+cleansheets:0,
+value:"£0"
+},
+
+"diablo.tg":{
+rank:"Bronze I",
+position:"Attacker",
+points:0,
+goals:0,
+assists:0,
+blocks:0,
+cleansheets:0,
+value:"£0"
+},
+
+"DM.Vrfs":{
+rank:"Bronze I",
+position:"Defender",
+points:0,
+goals:0,
+assists:0,
+blocks:0,
+cleansheets:0,
+value:"£0"
+},
+
+"Dutch.":{
+rank:"Bronze I",
+position:"Midfielder",
+points:0,
+goals:0,
+assists:0,
+blocks:0,
+cleansheets:0,
+value:"£0"
+},
+
+"chico":{
+rank:"Bronze I",
+position:"Attacker",
+points:0,
+goals:0,
+assists:0,
+blocks:0,
+cleansheets:0,
+value:"£0"
+},
+
+"vinassotuff":{
+rank:"Bronze I",
+position:"Defender",
+points:0,
+goals:0,
+assists:0,
+blocks:0,
+cleansheets:0,
+value:"£0"
+},
+
+"Arditaaaa":{
+rank:"Bronze I",
+position:"Midfielder",
+points:0,
+goals:0,
+assists:0,
+blocks:0,
+cleansheets:0,
+value:"£0"
+},
+
+"Zta.":{
+rank:"Bronze I",
+position:"Attacker",
+points:0,
+goals:0,
+assists:0,
+blocks:0,
+cleansheets:0,
+value:"£0"
+},
+
+"Goob":{
+rank:"Bronze I",
+position:"Attacker",
+points:0,
+goals:0,
+assists:0,
+blocks:0,
+cleansheets:0,
+value:"£0"
+},
+
+"el bicho":{
+rank:"Bronze I",
+position:"Defender",
+points:0,
+goals:0,
+assists:0,
+blocks:0,
+cleansheets:0,
+value:"£0"
+},
+
+"Hybrid9111":{
+rank:"Bronze I",
+position:"Midfielder",
+points:0,
+goals:0,
+assists:0,
+blocks:0,
+cleansheets:0,
+value:"£0"
+},
+
+"Hyper. Vrfs pro":{
+rank:"Bronze I",
+position:"Attacker",
+points:0,
+goals:0,
+assists:0,
+blocks:0,
+cleansheets:0,
+value:"£0"
+},
+
+"Brody":{
+rank:"Bronze I",
+position:"Defender",
+points:0,
+goals:0,
+assists:0,
+blocks:0,
+cleansheets:0,
+value:"£0"
+},
+
+"JB-KAFZ":{
+rank:"Bronze I",
+position:"Goalkeeper",
+points:0,
+goals:0,
+assists:0,
+blocks:0,
+cleansheets:0,
+value:"£0"
+},
+
+"JT.vrfs":{
+rank:"Bronze I",
+position:"Defender",
+points:0,
+goals:0,
+assists:0,
+blocks:0,
+cleansheets:0,
+value:"£0"
+},
+
+"ZZZ":{
+rank:"Bronze I",
+position:"Midfielder",
+points:0,
+goals:0,
+assists:0,
+blocks:0,
+cleansheets:0,
+value:"£0"
+},
+
+"Kylian mbappe":{
+rank:"Bronze I",
+position:"Attacker",
+points:0,
+goals:0,
+assists:0,
+blocks:0,
+cleansheets:0,
+value:"£0"
+},
+
+"Lucas":{
+rank:"Bronze I",
+position:"Midfielder",
+points:0,
+goals:0,
+assists:0,
+blocks:0,
+cleansheets:0,
+value:"£0"
+},
+
+"ET9":{
+rank:"Bronze I",
+position:"Attacker",
+points:0,
+goals:0,
+assists:0,
+blocks:0,
+cleansheets:0,
+value:"£0"
+},
+
+"MEMOPRIMEWC":{
+rank:"Bronze I",
+position:"Midfielder",
+points:0,
+goals:0,
+assists:0,
+blocks:0,
+cleansheets:0,
+value:"£0"
+},
+
+"AntonioTheSt.":{
+rank:"Bronze I",
+position:"Defender",
+points:0,
+goals:0,
+assists:0,
+blocks:0,
+cleansheets:0,
+value:"£0"
+},
+
+"Yamal?":{
+rank:"Bronze I",
+position:"Attacker",
+points:0,
+goals:0,
+assists:0,
+blocks:0,
+cleansheets:0,
+value:"£0"
+},
+
+"Foden":{
+rank:"Bronze I",
+position:"Midfielder",
+points:0,
+goals:0,
+assists:0,
+blocks:0,
+cleansheets:0,
+value:"£0"
+},
+
+"Ghost=Him":{
+rank:"Bronze I",
+position:"Attacker",
+points:0,
+goals:0,
+assists:0,
+blocks:0,
+cleansheets:0,
+value:"£0"
+},
+
+"qmxrk":{
+rank:"Bronze I",
+position:"Defender",
+points:0,
+goals:0,
+assists:0,
+blocks:0,
+cleansheets:0,
+value:"£0"
+},
+
+"DEVVYSNTNXVA":{
+rank:"Bronze I",
+position:"Goalkeeper",
+points:0,
+goals:0,
+assists:0,
+blocks:0,
+cleansheets:0,
+value:"£0"
+},
+
+"inzagi":{
+rank:"Bronze I",
+position:"Attacker",
+points:0,
+goals:0,
+assists:0,
+blocks:0,
+cleansheets:0,
+value:"£0"
+},
+
+"wokethecm.":{
+rank:"Bronze I",
+position:"Midfielder",
+points:0,
+goals:0,
+assists:0,
+blocks:0,
+cleansheets:0,
+value:"£0"
+},
+
+"Saiku":{
+rank:"Bronze I",
+position:"Defender",
+points:0,
+goals:0,
+assists:0,
+blocks:0,
+cleansheets:0,
+value:"£0"
+},
+
+"rifleken":{
+rank:"Bronze I",
+position:"Attacker",
+points:0,
+goals:0,
+assists:0,
+blocks:0,
+cleansheets:0,
+value:"£0"
+},
+
+"sky":{
+rank:"Bronze I",
+position:"Goalkeeper",
+points:0,
+goals:0,
+assists:0,
+blocks:0,
+cleansheets:0,
+value:"£0"
+},
+
+"Snowzzz":{
+rank:"Bronze I",
+position:"Midfielder",
+points:0,
+goals:0,
+assists:0,
+blocks:0,
+cleansheets:0,
+value:"£0"
+},
+
+"ashtronaut":{
+rank:"Bronze I",
+position:"Attacker",
+points:0,
+goals:0,
+assists:0,
+blocks:0,
+cleansheets:0,
+value:"£0"
+},
+
+"ATOMIC":{
+rank:"Bronze I",
+position:"Defender",
+points:0,
+goals:0,
+assists:0,
+blocks:0,
+cleansheets:0,
+value:"£0"
+},
+
+"type":{
+rank:"Bronze I",
+position:"Midfielder",
+points:0,
+goals:0,
+assists:0,
+blocks:0,
+cleansheets:0,
+value:"£0"
+},
+
+"Vortex":{
+rank:"Bronze I",
+position:"Attacker",
+points:0,
+goals:0,
+assists:0,
+blocks:0,
+cleansheets:0,
+value:"£0"
+},
+
+"slax":{
+rank:"Bronze I",
+position:"Defender",
+points:0,
+goals:0,
+assists:0,
+blocks:0,
+cleansheets:0,
+value:"£0"
+},
+
+"xd234jsjs":{
+rank:"Bronze I",
+position:"Goalkeeper",
+points:0,
+goals:0,
+assists:0,
+blocks:0,
+cleansheets:0,
+value:"£0"
+},
+
+"Joshwini":{
+rank:"Bronze I",
+position:"Midfielder",
+points:0,
+goals:0,
+assists:0,
+blocks:0,
+cleansheets:0,
+value:"£0"
+},
+
+"ZanzTG.":{
+rank:"Bronze I",
+position:"Attacker",
+points:0,
+goals:0,
+assists:0,
+blocks:0,
+cleansheets:0,
+value:"£0"
+}
+
+};
