b71c051b3a9b1bade9dd0085bb74fbbb7f21419f
rpsgvrfsstats <creepz1353@gmail.com>
2026-05-26 21:30:22 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const playerList =
 document.getElementById("playerList");
 
@@ -17,6 +16,10 @@ const positions = [
 "Attacker",
 "Striker"
 ];
+
+/* HOMEPAGE */
+
+if(playerList){
 
 players.forEach((player,index)=>{
 
@@ -111,3 +114,154 @@ card.style.display="none";
 });
 
 });
+
+}
+
+/* PLAYER PROFILE PAGE */
+
+const params =
+new URLSearchParams(window.location.search);
+
+const player =
+params.get("name");
+
+const profileContainer =
+document.getElementById("profileContainer");
+
+if(profileContainer && player){
+
+profileContainer.innerHTML = `
+
+<div class="profile-card">
+
+<div class="top-profile">
+
+<div class="profile-pic"></div>
+
+<div>
+
+<div class="big-name">
+${player}
+</div>
+
+<div class="player-rank">
+Bronze I
+</div>
+
+</div>
+
+</div>
+
+<div class="tags">
+
+<div class="tag">
+⚽ Player
+</div>
+
+<div class="tag">
+🌎 NA
+</div>
+
+<div class="tag">
+🏆 RPSG
+</div>
+
+</div>
+
+<div class="main-stats">
+
+<div class="big-stat-card">
+
+<div class="stat-title">
+POINTS
+</div>
+
+<div class="big-number">
+0
+</div>
+
+</div>
+
+<div class="big-stat-card">
+
+<div class="stat-title">
+MARKET VALUE
+</div>
+
+<div class="big-number">
+£0
+</div>
+
+</div>
+
+</div>
+
+<div class="progress-section">
+
+<div class="stat-title">
+RANK PROGRESS
+</div>
+
+<div class="progress-bar">
+<div class="progress-fill"></div>
+</div>
+
+</div>
+
+<div class="small-grid">
+
+<div class="small-card">
+
+<div class="small-title">
+GOALS
+</div>
+
+<div class="small-number">
+0
+</div>
+
+</div>
+
+<div class="small-card">
+
+<div class="small-title">
+ASSISTS
+</div>
+
+<div class="small-number">
+0
+</div>
+
+</div>
+
+<div class="small-card">
+
+<div class="small-title">
+BLOCKS
+</div>
+
+<div class="small-number">
+0
+</div>
+
+</div>
+
+<div class="small-card">
+
+<div class="small-title">
+CLEAN SHEETS
+</div>
+
+<div class="small-number">
+0
+</div>
+
+</div>
+
+</div>
+
+</div>
+
+`;
+
+}
